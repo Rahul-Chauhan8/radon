4f6677b2bc47bf0913094d6eb8a02a99ab5c53b4
“Rahul <ranarahul8889@gmail.com>
2022-06-04 20:20:53 +0530

    hi

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://RahulChauhan:3aDm5xdCx8MiuHql@cluster0.xzyyibs.mongodb.net/rahul1995?retryWrites=true&w=majority",{
+ useNewUrlParser:true   
+})
+
+.then ( () => console.log("mongodb is connected"))
+ .catch (err => console.log(err))
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
